497fdf36a5ffef2e4dbd0e0ce095eca5c0a2bca1
quanyb98 <32408293+quanyb98@users.noreply.github.com>
2021-05-30 11:01:03 +0700

    Update 2_deploy_contracts.js

--- screencast/310-flashloan-bsc/migrations/2_deploy_contracts.js
+++ screencast/310-flashloan-bsc/migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const Arbitrage = artifacts.require("Arbitrage.sol");
 module.exports = function (deployer) {
   deployer.deploy(
     Arbitrage,
-    '0xBCfCcbde45cE874adCB698cC183deBcF17952812', //PancakeSwap factory
-    'couldlt find their address :( if you try to trade with a liquidity pool on their website, metamask should show you the address of their router', //BakerySwap router
+    '0xBCfCcbde45cE874adCB698cC183deBcF17952812', //PancakeSwapV1 factory
+    '0x10ED43C718714eb63d5aA57B78B54704E256024E#code', //BakerySwapV2 router
   );
 };
